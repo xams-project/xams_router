f1fcc613952b16bfe6a76f1494a4d7d92c772100
Dom Rodriguez <shymega@shymega.org.uk>
2017-05-28 17:24:21 +0100

    Initial commit

---
+++ lib/acom_xp_router.ex
@@ -0,0 +1,18 @@
+defmodule AcomXpRouter do
+  @moduledoc """
+  Documentation for AcomXpRouter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AcomXpRouter.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/acom_xp_router_test.exs
@@ -0,0 +1,8 @@
+defmodule AcomXpRouterTest do
+  use ExUnit.Case
+  doctest AcomXpRouter
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
