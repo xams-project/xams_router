437ec4dcb5a3aed6448ac285df8a066f080710ba
Dom Rodriguez <shymega@shymega.org.uk>
2017-07-10 15:38:35 +0100

    Update XAMS to use the Erlang MQTT library
    This is a WORK IN PROGRESS and SHOULD NOT be used yet!!

--- lib/xams_router/inputs/mqtt.ex
+++ lib/xams_router/inputs/mqtt.ex
@@ -1,41 +1,58 @@
 defmodule XAMS.Inputs.MQTT do
   require Logger
+  use GenServer
 
-  def start_link() do
-    GenServer.start_link(__MODULE__, [])
+  @name __MODULE__
+
+  def start_link(topics) do
+    GenServer.start_link(@name, topics, name: @name)
   end
 
-  def init(_argv) do
-    Process.register(self(), :testclient)
+  def init(topics) do
+    {:ok, conn} = :emqttc.start_link(host: '127.0.0.1',
+      logger: :error)
     
-    {:ok, pid} = Exmqttc.start_link(Exmqtt.Testclient, [name: :xams],
-      host: '127.0.0.1')
-    
-    Exmqttc.subscribe(:xams, "xams/#")
-    {:ok, pid}
+    Logger.info("MQTT client now attempting to connect to broker.")
+    {:ok, %{conn: conn,
+           topics: topics}}
   end
-  
-  def handle_info(:connected, state) do
-    Logger.info("Connected to MQTT broker.")
+
+  def handle_info({:mqttc, conn, :connected}, %{conn: conn,
+                                                topics: topics} = state) do
+    Logger.info("Success. MQTT client connected!")
+
+    # Map over topic list and subscribe.
+    # Stock configuration is `["xams/#"], which subscribes to *all*
+    # topics under that namespace.
+    Enum.map(topics, fn (t) ->
+      Logger.debug("Router about to subscribe to topic -> #{t}")
+      :emqttc.subscribe(conn, t, :qos0)
+      Logger.debug("Router now subscribed to topic -> #{t}")
+    end)
+
+    Logger.info("Finished subscribing phase.")
     {:noreply, state}
   end
-  def handle_info({:publish, topic, msg}, state) do
-    Logger.debug("Received message on Topic: #{topic}")
-    Logger.debug("Message: #{msg}")
-    msg = Poison.decode!(msg)
-    c_ver = msg |> get_in(["xams",
-                           "client",
-                           "version"]) # XAMS client version.
-    c_uuid = msg |> get_in(["xams",
-                            "client",
-                            "uuid"]) # XAMS client UUID.
-
-    Logger.debug("Client (UUID: #{c_uuid}) interacted with us...")
-    Logger.debug("Client version: #{c_ver}")
+  def handle_info({:mqttc, conn, :disconnected}, %{conn: conn,
+                                                   topics: _topics} = state) do
+    Logger.info("MQTT client disconnected. Network failure?")
     {:noreply, state}
   end
-  def handle_info(:disconnected, state) do
-    Logger.error("Disconnected from MQTT broker.")
+  def handle_info({:publish, topic, payload}, %{conn: _conn,
+                                                topics: _topics} = state) do
+
+    Logger.debug("Received a new message from the broker.")
+    Logger.debug("Topic -> #{topic}")
+    Logger.debug("Payload -> #{payload}")
+
+    json_body = Poison.decode!(payload)
+
+    device_uuid = get_in(json_body,
+      ["identity",
+       "uuid"])
+    
+    Logger.debug("Router received a CONNREQ from a device.\nDevice UUID -> #{device_uuid}")
+
     {:noreply, state}
   end
 end
--- lib/xams_router/supervisors/supervisor.ex
+++ lib/xams_router/supervisors/supervisor.ex
@@ -8,16 +8,20 @@ defmodule XAMS.Supervisors.Supervisor do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    xamsTopics = Application.get_env(:xams,
+      :topics)
+
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: XAMS.Worker.start_link(arg1, arg2, arg3)
       # worker(XAMS.Worker, [arg1, arg2, arg3]),
-      worker(XAMS.Inputs.MQTT, [])
-    ]
+      worker(XAMS.Inputs.MQTT, [xamsTopics])]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: XAMS.Supervisor]
+    opts = [strategy: :one_for_one, name: XAMS.Supervisor,
+            max_restarts: 45,
+            max_seconds: 10]
     Supervisor.start_link(children, opts)
   end
 end
