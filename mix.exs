e1ed4b8ac66cfe59b2f72d40979ab7983b8d6ae2
Dom Rodriguez <shymega@shymega.org.uk>
2017-07-10 15:37:32 +0100

    Add IRC and XMPP libs to Application list
    This commit ensures that the IRC and XMPP libraries are ready when the
    app is launched.

--- mix.exs
+++ mix.exs
@@ -15,7 +15,9 @@ defmodule XAMS.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger,
+                          :romeo,
+                          :exirc],
      mod: {XAMS.Supervisors.Supervisor, []}]
   end
 
