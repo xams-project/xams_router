348d8894843a1b8c09af8cc10f59d82e925c99be
Dom Rodriguez <shymega@shymega.org.uk>
2017-08-15 17:03:40 +0100

    Change `Inputs` namespace to "MQTT"

--- lib/xams_router/mqtt/mqtt.ex
+++ lib/xams_router/mqtt/mqtt.ex
@@ -1,26 +1,47 @@
-defmodule XAMS.Inputs.MQTT do
+defmodule XAMSRouter.MQTT.MQTT do
   require Logger
   use GenServer
 
-  @name __MODULE__
-
   def start_link(topics) do
-    GenServer.start_link(@name, topics, name: @name)
+    GenServer.start_link(__MODULE__, topics, name: __MODULE__)
   end
-
+  
   def init(topics) do
-    {:ok, conn} = :emqttc.start_link(host: '127.0.0.1',
-      logger: :error)
-    
+    app_env = Application.get_env(:xams, :mqtt)
+    host = app_env[:host]
+    port = app_env[:port]
+    ssl? = app_env[:ssl?]
+    client_id = app_env[:client_id]
+    username = app_env[:username]
+    password = app_env[:password]
+
+    {:ok, conn} = if ssl? do
+      Logger.debug("Connecting with SSL.")
+      :emqttc.start_link([{:host, host},
+                          {:port, port},
+                          :ssl,
+                          {:username, username},
+                          {:password, password},
+                          {:client_id, client_id}])
+    else
+      Logger.debug("Connecting without SSL.")
+      :emqttc.start_link([{:host, host},
+                          {:port, port},
+                          {:username, username},
+                          {:password, password},
+                          {:client_id, client_id}])
+    end
+
     Logger.info("MQTT client now attempting to connect to broker.")
+    
     {:ok, %{conn: conn,
             topics: topics}}
   end
-
+  
   def handle_info({:mqttc, conn, :connected}, %{conn: conn,
                                                 topics: topics} = state) do
     Logger.info("Success. MQTT client connected!")
-
+    
     # Map over topic list and subscribe.
     # Stock configuration is `["xams/#"], which subscribes to *all*
     # topics under that namespace.
@@ -44,15 +65,14 @@ defmodule XAMS.Inputs.MQTT do
     Logger.debug("Received a new message from the broker.")
     Logger.debug("Topic -> #{topic}")
     Logger.debug("Payload -> #{payload}")
-
-    json_body = Poison.decode!(payload)
-
-    device_uuid = get_in(json_body,
-      ["identity",
-       "uuid"])
     
-    Logger.debug("Router received a CONNREQ from a device.\nDevice UUID -> #{device_uuid}")
+    payload = Poison.decode!(payload)
+    
+    device_uuid = get_in(payload,
+      ["identity",
+       "deviceUUID"])
 
+    Logger.debug("Router received a payload from device (UUID) -> #{device_uuid}")
     {:noreply, state}
   end
 end
