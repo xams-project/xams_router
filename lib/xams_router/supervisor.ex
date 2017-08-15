188f20dc399b1b360fe9c314d7a3af49916b3bb0
Dom Rodriguez <shymega@shymega.org.uk>
2017-08-15 17:04:08 +0100

    Remove `Supervisor` directory

--- lib/xams_router/supervisor.ex
+++ lib/xams_router/supervisor.ex
@@ -1,4 +1,4 @@
-defmodule XAMS.Supervisors.Supervisor do
+defmodule XAMSRouter.Supervisor do
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
@@ -9,19 +9,14 @@ defmodule XAMS.Supervisors.Supervisor do
     import Supervisor.Spec, warn: false
 
     xamsTopics = Application.get_env(:xams,
-      :topics)
+     :mqtt)[:topics]
 
     # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: XAMS.Worker.start_link(arg1, arg2, arg3)
-      # worker(XAMS.Worker, [arg1, arg2, arg3]),
-      worker(XAMS.Inputs.MQTT, [xamsTopics])]
+    children = [worker(XAMSRouter.MQTT.MQTT, [xamsTopics])]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: XAMS.Supervisor,
-            max_restarts: 45,
-            max_seconds: 10]
+    opts = [strategy: :one_for_one, name: XAMSRouter.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
