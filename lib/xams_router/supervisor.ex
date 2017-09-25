4055cae91f60151e5121ce373fb20e0b12f82980
Dom Rodriguez <shymega@shymega.org.uk>
2017-09-25 15:52:52 +0100

    Fix whitespace

--- lib/xams_router/supervisor.ex
+++ lib/xams_router/supervisor.ex
@@ -7,7 +7,7 @@ defmodule XAMSRouter.Supervisor do
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-    
+
     # Define workers and child supervisors to be supervised
     children = [
       worker(XAMSRouter.MQTT.Client, [])
